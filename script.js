b9fcbbf6d7b4baef50e651698e07e1fcbbf7d333
isaac dyess <isaac.dyess@gmail.com>
2020-05-28 15:15:10 -0500

    Added options for percentage rather than only entered values.

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
 function tipCalculate() {
+  //get inputted cost from index
   var cost = document.getElementById("cost").value;
-  var tip_pct = document.getElementById("tip_pct").value;
-  var tip_total = cost * (tip_pct / 100);
+
+  //get selected percentage as string
+  var tip_pct = document.getElementById("pct_value").value;
+
+  //get decimal value from tip_pct
+  tip_pct = parseFloat(tip_pct) / 100;
+
+  //calculate the tip
+  var tip_total = cost * tip_pct;
+  //format tip to 2 decimal spaces
   tip_total = tip_total.toFixed(2);
-  document.getElementById("output").innerHTML = "$" + tip_total;
-  //alert(tip_total);
+  //output the tip total if not NaN
+  if (!isNaN(tip_total)){
+    document.getElementById("output").innerHTML = "$" + tip_total;
+  }
+  else{
+    alert("Enter a valid price.");
+  }
 }
